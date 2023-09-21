2aff9abe9e8ae765c60f96e66122b5644bcc3f7e
warrior-JD <juandiguzman69@gmail.com>
2023-09-20 19:36:58 -0500

    Primer commit
    primer commit 20/09/2023

---
+++ src/main/java/co/edu/uco/tiendaonline/initializer/TiendaOnlineApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.tiendaonline.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaOnlineApplication.class, args);
+	}
+
+}
