f0f943ff64bf2cd33e18feced3998fabee034142
warrior-JD <juandiguzman69@gmail.com>
2023-09-27 19:36:24 -0500

    segundo commit
    clase 27/09/2023

--- src/main/java/co/edu/uco/tiendaonline/data/dao/daofactory/DAOFactory.java
+++ src/main/java/co/edu/uco/tiendaonline/data/dao/daofactory/DAOFactory.java
@@ -1,5 +1,22 @@
 package co.edu.uco.tiendaonline.data.dao.daofactory;
 
-public class DAOFactory {
+import co.edu.uco.tiendaonline.data.dao.ClienteDAO;
+import co.edu.uco.tiendaonline.data.dao.TipoIdentificacionDAO;
+
+public abstract class DAOFactory {
+	
+	protected abstract void abirConexion();
+	
+	public abstract void cerrarConexion();
+	
+	public abstract void iniciarTransaccion();
+	
+	public abstract void confirmarTransaccion();
+	
+	public abstract void cancelarTransaccion();
+	
+	public abstract ClienteDAO  obtenerClienteDAO();
+	
+	public abstract TipoIdentificacionDAO obtenerTipoIdentificacionDAO();
 
 }
